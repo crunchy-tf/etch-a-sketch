7cac8b032235f97faf5e2fa030a8dddb916600ca
Ayoub <ayoubdhaouadi08@gmail.com>
2022-12-04 08:22:12 +0100

    Added board logic and cell coloring

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ slider = document.querySelector('.slider')
 grid = document.querySelector('.grid')
 
 slider.addEventListener('change', () => {
-    slider_values = [4,8,16,32,64]
+    slider_values = [8,16,32,64,128]
+    cell_sizes = [50,25,12.5,6.25,3.125]
     size = slider_values[slider.value]
-
+    grid.innerHTML = ''
     for (i=0;i<size*size;i++){
         cell = document.createElement('div')
-        cell.innerText = i+1
         cell.className = "cell"
         grid.appendChild(cell)
+        cell.style.width = `${cell_sizes[slider.value]}px`
+        cell.style.height = `${cell_sizes[slider.value]}px`
     }
+    
+    grid.style.gridTemplateColumns = `repeat(${slider_values[slider.value]},1fr)`
+
+    cells = document.querySelectorAll('.cell')
+
+    cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black'
+    })
+});
 })
+
